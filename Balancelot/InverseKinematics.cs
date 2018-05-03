d6d6c52e938e3749eb7cd6fd6be45e77c48d5523
Tuomas Männistö <36795843+Torppe@users.noreply.github.com>
2018-05-03 11:00:49 +0300

    Add files via upload
    These were meant to be used in this project, but it later turned out that we don't need them.

---
+++ Balancelot/InverseKinematics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseKinematics : MonoBehaviour {
+
+	public Limb[] limbs;
+	public Transform basePoint;
+
+	private void Update()
+	{
+		for(int i = limbs.Length-1; i >= 0; i--)
+		{
+			limbs[i].ConnectLimb();
+		}
+
+		limbs[0].transform.position = basePoint.position;
+
+		for(int i = 1; i < limbs.Length; i++)
+		{
+			limbs[i].transform.position = limbs[i - 1].endJoint.position;
+		}
+	}
+}
---
+++ Balancelot/Limb.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Limb : MonoBehaviour {
+
+	public Transform connectionPoint;
+	[HideInInspector]
+	public Transform endJoint;
+
+	private Vector2 direction;
+	private Vector3 positionFromPoint;
+
+	Quaternion rotationQ;
+
+	private float objectLength;
+	private float angle_;
+
+	void Start()
+	{
+		endJoint = gameObject.transform.Find("EndJoint");
+		objectLength = (endJoint.position - transform.position).magnitude;
+	}
+
+	public void ConnectLimb()
+	{
+		direction = (connectionPoint.position - transform.position).normalized;
+
+		angle_ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		rotationQ = Quaternion.AngleAxis(angle_, transform.forward);
+		transform.rotation = Quaternion.Lerp(transform.rotation, rotationQ, 1);
+
+		positionFromPoint = direction * objectLength * -1;
+		transform.position = connectionPoint.position + positionFromPoint;
+
+	}
+}
